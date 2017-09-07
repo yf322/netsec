7bb135b6a67e409919d8418749cda9b7df63e866
Yongqiang Fan <fldclassicblue@gmail.com>
2017-09-06 20:17:59 -0400

    unit test

--- netsec_fall2017/lab1_b/submission.py
+++ netsec_fall2017/lab1_b/submission.py
@@ -1,18 +1,60 @@
 from playground.network.packet import PacketType
 from loginUsername import LogInWithUsername
 from loginStatus import LogInStatus
-from getUserProfile import GetUserProfileWithID
+from getUserProfile import GetUserProfleWithID
 from sendUserProfile import SendUserProfile
 
-packet1 = LogInWithUsername()
-packet1.username = "Yongqiang Fan"
-packet1.password = b"There are a lot of works in lab2b than lab2a."
+def packet1Test():
+    packet1 = LogInWithUsername()
+    packet1.username = "Yongqiang Fan"
+    packet1.password = "password"
 
-packetBytes = packet1.__serialize__()
+    packet1Bytes = packet1.__serialize__()
+    packet1a = LogInWithUsername.Deserialize(packet1Bytes)
+    assert packet1 == packet1a
 
-packet2 = PacketType.Deserialize(packetBytes)
-if packet1 == packet2:
-	print("yes")
+def packet2Test():
+    packet2 = LogInStatus()
+    packet2.status = True
+    packet2.userID = "1"
+
+    packet2Bytes = packet2.__serialize__()
+    packet2a = LogInStatus.Deserialize(packet2Bytes)
+    assert packet2 == packet2a
+
+def packet3Test():
+    packet3 = GetUserProfleWithID()
+    packet3.userID = "1"
+
+    packet3Bytes = packet3.__serialize__()
+    packet3a = GetUserProfleWithID.Deserialize(packet3Bytes)
+    assert packet3 == packet3a
+
+def packet4Test():
+    packet4 = SendUserProfile()
+    packet4.profile = '{"username": "Yongqiang Fan", "userID": "1"}'
+
+    packet4Bytes = packet4.__serialize__()
+    packet4a = SendUserProfile.Deserialize(packet4Bytes)
+    assert packet4 == packet4a
+
+def basicUnitTest():
+    try:
+        packet1Test()
+        packet2Test()
+        packet3Test()
+        packet4Test()
+    except:
+        print("Unexpected error!")
+
+
+
+
+
+
+basicUnitTest()
+
+
 
 
 
